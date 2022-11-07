025d21556a98e9f15594c00d286e21ba5437fedd
Philani Ncube <ncbphi001@gmail.com>
2022-11-07 16:22:22 +0200

    container queries

---
+++ pages/container-queries.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {format, parse} from 'date-fns'
+
+const containerQueries = () => {
+  return (
+    <div>containerQueries</div>
+  )
+}
+
+export default containerQueries
